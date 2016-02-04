bfd0f60ee2341cd00a159d77dc6f61538471f2db
Mark J. Miller <developmentalmadness@gmail.com>
2016-02-03 17:28:09 -0700

    sample data changes to test fixes

--- radars/radarData.js
+++ radars/radarData.js
@@ -1,5 +1,5 @@
 //This is the title for your window tab, and your Radar
-document.title = "WotifGroup's Technology Radar (December 2014)";
+document.title = "inContact Orchestration Team Technology Radar (February 2016)";
 
 
 //This is the concentic circles that want on your radar
@@ -54,10 +54,14 @@ var radar_data = [
         "top" : 18,
         "color" : "#8FA227",
         "items" : [ 
-            { name: 'Git flow / Pull Requests ^', pc: { r: 230, t: 133 }, movement: 'c' },
-            {"name":"Incremental data warehousing", "pc":{"r":250,"t":165},"movement":"c"},    
             {"name":"Events for messages - CQRS", "pc":{"r":225,"t":120},"movement":"c"},
-            {"name":"Measure Pipeline disruptions", "pc":{"r":280,"t":110},"movement":"c"}, 
+            {"name":"Dependency Injection", "pc":{"r":280,"t":130},"movement":"c"},   
+            {"name":"Isolated dev envs", "pc":{"r":280,"t":110},"movement":"c"}, 
+
+
+
+
+/*
             {"name":"Continuous Experimentation", "pc":{"r":230,"t":110},"movement":"c"},
             { name: 'Reduce iRules dependence ^', pc: { r: 280, t: 133 }, movement: 'c' },
             {"name":"SaaS for non-core systems", "pc":{"r":170,"t":150},"movement":"c"},   
@@ -69,7 +73,6 @@ var radar_data = [
             {"name":"Data Informed Decion Making", "pc":{"r":130,"t":110},"movement":"c"},
             {"name":"Polygot Programming", "pc":{"r":180,"t":170},"movement":"c"},
             { name: 'internal load balancing off F5^', pc: { r: 180, t: 133 }, movement: 'c' },
-            {"name":"Isolated dev envs", "pc":{"r":180,"t":125},"movement":"c"},  
             {"name":"Edge Services", "pc":{"r":130,"t":160},"movement":"c"}, 
             {"name":"Clean Code", "pc":{"r":130,"t":120},"movement":"c"},
             {"name":"Wide and Thin Front-Ends", "pc":{"r":180,"t":160},"movement":"c"},
@@ -80,9 +83,8 @@ var radar_data = [
             {"name":"Valuable, cheap tests", "pc":{"r":130,"t":150},"movement":"c"},
             {"name":"Sacrificial Architecture", "pc":{"r":80,"t":100},"movement":"c"},   
             {"name":"Sensible defaults", "pc":{"r":80,"t":150},"movement":"c"},   
-            {"name":"Dependency Injection", "pc":{"r":80,"t":130},"movement":"c"},   
             {"name":"Coding architects", "pc":{"r":90,"t":170},"movement":"c"}
-
+*/
         ]
     },
     { "quadrant": "Tools",
@@ -91,15 +93,21 @@ var radar_data = [
         "color" : "#587486",
         "items" : [ 
 
-{ name: 'Docker', pc: { r: 170, t: 19 }, movement: 't' },
-  { name: 'bind',    pc: { r: 150, t: 69 },    movement: 'c' },
-  { name: 'Appium',    pc: { r: 110, t: 70 },    movement: 'c',    domain: 'mobile, front-end' }, 
- { name: 'Android Studio',    pc: { r: 180, t: 66 },    movement: 'c',    domain: 'mobile, dev' },
-  { name: 'Responsive Android',    pc: { r: 150, t: 14 },    movement: 'c' },
-  { name: 'AutoLayout - iOS',    pc: { r: 180, t: 55 },    movement: 'c',    domain: '' },
-  { name: 'Kiwi - iOS unit test',    pc: { r: 120, t: 14 },    movement: 'c',    domain: '' },
-  { name: 'BEM',    pc: { r: 160, t: 60 },    movement: 'c',    domain: 'front-end' },
-  { name: 'Crashlytics',    pc: { r: 180, t: 5 },    movement: 'c',    domain: 'mobile' },
+{ name: 'Docker', pc: { r: 70, t: 19 }, movement: 't' },
+  { name: 'Jenkins',    pc: { r: 50, t: 69 },    movement: 'c' },
+  { name: 'Zookeeper',    pc: { r: 210, t: 70 },    movement: 'c',    domain: 'back-end' }, 
+ { name: 'IntelliJ',    pc: { r: 80, t: 66 },    movement: 'c',    domain: 'dev' },
+  { name: 'Google Hangouts',    pc: { r: 250, t: 14 },    movement: 'c' },
+  { name: 'Gitter',    pc: { r: 80, t: 55 },    movement: 'c',    domain: 'communication' },
+  { name: 'Git',    pc: { r: 30, t: 73 },    movement: 'c' },
+  { name: 'Kafka',    pc: { r: 212, t: 25 },    movement: 'c',    domain: 'back-end' },
+  { name: 'ELK',    pc: { r: 230, t: 72 },    movement: 'c',    domain: 'back-end' },
+  { name: 'nginx',    pc: { r: 260, t: 60 },    movement: 'c',    domain: 'front-end' },
+  { name: 'Eclipse CHE',    pc: { r: 380, t: 5 },    movement: 'c',    domain: 'mobile' },
+  
+  
+  
+/*  
   { name: 'Consul',    pc: { r: 170, t: 29 },    movement: 't' }, 
    { name: 'Swagger Code-Gen',    pc: { r: 180, t: 82 },    movement: 'c' },
    { name: 'PowerMock ^',    pc: { r: 180, t: 46 },    movement: 'c' },
@@ -107,7 +115,6 @@ var radar_data = [
    { name: 'Json Web Tokens (JWT)',    pc: { r: 180, t: 77 },    movement: 'c' },
    { name: 'Lemming',    pc: { r: 160, t: 82 },    movement: 'c' },
    { name: 'Hystrix',    pc: { r: 150, t: 36 },    movement: 'c' },
-  { name: 'Git',    pc: { r: 130, t: 73 },    movement: 'c' },
 
   { name: 'Ansible',    pc: { r: 280, t: 74 },    movement: 'c' },  
   { name: 'Hip Chat',    pc: { r: 280, t: 78 },    movement: 'c' },
@@ -118,8 +125,6 @@ var radar_data = [
   { name: 'GenyMotion',    pc: { r: 210, t: 31 },    movement: 'c' },
 
   { name: 'JDBI ^',    pc: { r: 80, t: 56 },    movement: 'c' },
-  { name: 'Kafka',    pc: { r: 12, t: 25 },    movement: 'c',    domain: 'back-end' },
-  { name: 'ELK',    pc: { r: 30, t: 72 },    movement: 'c',    domain: 'back-end' },
   { name: 'Liquibase',    pc: { r: 80, t: 76 },    movement: 'c' },
   { name: 'haproxy',    pc: { r: 80, t: 46 },    movement: 'c' },
 
@@ -127,6 +132,7 @@ var radar_data = [
   { name: 'Hibernate ^',    pc: { r: 380, t: 56 },    movement: 'c' },
   { name: 'mongoDB',    pc: { r: 330, t: 5 },    movement: 'c' }, 
   { name: 'Subversion',    pc: { r: 330, t: 18 },    movement: 'c' }
+*/
   ]
     },
     { "quadrant": "Platforms",
@@ -135,8 +141,15 @@ var radar_data = [
         "color" : "#DC6F1D",
         "items" : [
 
-            {"name":"OpenId Connect", "pc":{"r":130,"t":260},"movement":"t"},   
-            {"name":"Location based services", "pc":{"r":130,"t":230},"movement":"c"},
+            {"name":"Github", "pc":{"r":30,"t":260},"movement":"t"},
+            {"name":"JVM as platform", "pc":{"r":90,"t":265},"movement":"c"},   
+            {"name":"AWS", "pc":{"r":90,"t":250},"movement":"c"},   
+            { name: 'Mesos',              pc: { r: 260, t: 265 },              movement: 't' },
+            { name: 'Kubernetes',              pc: { r: 270, t: 236 },              movement: 't' },
+
+
+            
+/*            
             {"name":"Openstack", "pc":{"r":190,"t":190},"movement":"c"},
             {"name":"RHEL 7", "pc":{"r":170,"t":215},"movement":"c"},      
 
@@ -145,16 +158,11 @@ var radar_data = [
             { name: 'Postgres as NoSQL',              pc: { r: 220, t: 255 },              movement: 'c' },
             {"name":"AWS 2014 Innovations", "pc":{"r":270,"t":195},"movement":"c"},
             {"name":"Azure", "pc":{"r":290,"t":265},"movement":"c"},   
-            { name: 'Mesos',              pc: { r: 260, t: 265 },              movement: 't' },
-            { name: 'Marathon',              pc: { r: 240, t: 268 },              movement: 't' },
-            { name: 'Kubernetes',              pc: { r: 270, t: 236 },              movement: 't' },
             {"name":"Google App Engine", "pc":{"r":290,"t":255},"movement":"c"},   
             {"name":"Google as corporate platform", "pc":{"r":290,"t":200},"movement":"c"},   
 
 
             {"name":"Google Play - (alpha/beta builds)", "pc":{"r":30,"t":225},"movement":"c"},
-            {"name":"JVM as platform", "pc":{"r":90,"t":265},"movement":"c"},   
-            {"name":"AWS", "pc":{"r":90,"t":250},"movement":"c"},   
             { name: 'BigIP v11',              pc: { r: 50, t: 257 },              movement: 'c' },
 
 
@@ -165,7 +173,7 @@ var radar_data = [
             {"name":"Java EE - the Bad Parts", "pc":{"r":390,"t":245},"movement":"c"},   
             {"name":"MS SqlServer", "pc":{"r":390,"t":190},"movement":"c"},
             {"name":"RHEL 5", "pc":{"r":370,"t":195},"movement":"c"}
-
+*/
         ]
     },
     { "quadrant": "Languages & Frameworks",
@@ -173,13 +181,20 @@ var radar_data = [
         "left"  : (w-200+30),
         "top" :   (h/2 + 18),
         "items" : [ 
-            { name: 'CDI', pc: { r: 60, t: 290 },  movement: 'c' },
-            { name: 'Jersey', pc: { r: 60, t: 310 },  movement: 'c' },
+            { name: 'Scala', pc: { r: 60, t: 290 },  movement: 'c' },
+            { name: 'Akka', pc: { r: 60, t: 310 },  movement: 'c' },
 
-            { name: 'Guice', pc: { r: 60, t: 278 },  movement: 'c' },
-            { name: 'RxJava', pc: { r: 150, t: 298 },              movement: 'c',  domain: 'template' },          
+            { name: 'Akka Streams', pc: { r: 260, t: 278 },  movement: 'c' },
+            { name: 'Play!', pc: { r: 150, t: 298 },              movement: 'c',  domain: 'template' },          
 
-            {"name":"Java 8", "pc":{"r":130,"t":355},"movement":"c"},   
+            {"name":"Akka Http", "pc":{"r":230,"t":355},"movement":"c"},
+            
+            
+            
+            
+            
+  /*          
+               
             {"name":"Groovy ^", "pc":{"r":190,"t":280},"movement":"c"},
 
             {"name":"Swift", "pc":{"r":280,"t":300},"movement":"c"},
@@ -196,6 +211,7 @@ var radar_data = [
             {"name":"Web Objects", "pc":{"r":390,"t":290},"movement":"c"},
             {"name":"ASP Classic", "pc":{"r":375,"t":330},"movement":"c"},
             {"name":"Java 6 and earlier", "pc":{"r":390,"t":350},"movement":"c"}
+*/
         ]
     }
 ];
